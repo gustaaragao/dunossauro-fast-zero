920706717603b19ed75d9661f36abfeae2c6c3f0
gustaaragao <gustavohenriquearagaosilva@gmail.com>
2024-06-26 19:04:30 -0300

    Adicionada a primeira migração com Alembic. Criada tabela de usuários.

---
+++ fast_zero/models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, registry
+
+table_registry = registry()  # Registra "metadados" da Tabela (DB)
+
+"""
+Mapeia para uma "dataclass"
+dataclass -> Estrutura do Python que é uma Classe com *APENAS* atributos(dados)
+"""
+
+
+@table_registry.mapped_as_dataclass
+class User:
+    # __tablename__ -> Nome da Tabela
+    __tablename__ = 'users'
+
+    # Mapped -> o SQLAlchemy vai se "virar" para mapeiar isso para o SQL
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str]
+    password: Mapped[str] = mapped_column(unique=True)
+    created_at: Mapped[datetime] = mapped_column(
+        init=False,
+        server_default=func.now(),  # O 'now' é do Servidor
+    )
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,31 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from fast_zero.app import app
+from fast_zero.models import table_registry
 
 
 @pytest.fixture()
 def client():
     return TestClient(app)
+
+
+@pytest.fixture()
+def session():
+    # Conexão com o banco de dados (sqlite3)
+    # :memory: -> cria DB em Memória
+    engine = create_engine('sqlite:///:memory:')
+
+    """
+    table_registry conhece todas as tabelas (metadados)
+    |-> criar todas as tabelas a partir da Engine
+    """
+    table_registry.metadata.create_all(engine)
+
+    # with -> gerenciamento de contexto
+    with Session(engine) as session:
+        yield session  # Geradores/Corrotinas
+
+    table_registry.metadata.drop_all(engine)  # Destruir o DB
