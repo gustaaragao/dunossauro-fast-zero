2641054ede79f154e53390bcd24c383d1ff87376
Gustavo Henrique <gustavohenriquearagaosilva@gmail.com>
2025-03-13 08:20:22 -0300

    limpando repo

--- tests/test_app.py
+++
@@ -1,9 +0,0 @@
-from http import HTTPStatus
-
-
-def test_root(client):
-    response = client.get('/')
-
-    assert response.status_code == HTTPStatus.OK
-
-    assert response.json() == {'message': 'Olá Mundo!'}
