fab61f295015f31e066c365ada29323ddaf046da
Gustavo Henrique <gustavohenriquearagaosilva@gmail.com>
2025-03-19 09:44:23 -0300

    Criacao da tabela usuarios

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
 
 from zero.app import app
@@ -14,7 +17,7 @@ def client():
 
 @pytest.fixture
 def session():
-    # Cria a Engine em Memória para o banco para criar a Sessão
+    # Cria uma Engine em Memória para o banco
     engine = create_engine('sqlite:///:memory:')
 
     # Cria todas tabelas registradas no registry
@@ -25,6 +28,26 @@ def session():
         # Injeta uma instância de Session
         yield session
 
-    table_registry.metadata.drop_all(engine)
     # Após cada teste todas as tabelas do banco são eliminadas
     # -> Garante independência entre os testes
+    table_registry.metadata.drop_all(engine)
+
+
+@contextmanager  # Cria um gerenciador de contexto (with)
+# * garante que todos os parâmetros sejam chamados de forma explícita
+def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
+    # hook(mapper, connection, target)
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,21 +1,28 @@
+from dataclasses import asdict
+
 from sqlalchemy import select
 
 from zero.models import User
 
 
-def test_create_user(session):
-    new_user = User(
-        username='Gustavo', email='gustavo@email.com', password='secret'
-    )
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='Gustavo', email='gustavo@email.com', password='secret'
+        )
 
-    # O usuário está na Sessão, mas não no Banco (ESTADO TRANSIENTE)
-    # --> Padrão: Unidade de Trabalho
-    session.add(new_user)
-    # Performar as ações no Banco
-    session.commit()
+        # O usuário está na Sessão, mas não no Banco (ESTADO TRANSIENTE)
+        # --> Padrão: Unidade de Trabalho
+        session.add(new_user)
+        # Performar as ações no Banco
+        session.commit()
 
-    user = session.scalar(select(User).where(User.username == 'Gustavo'))
-    # .scalar() -> Pega o primeiro resultado da Query e converte para um objeto
-    # select, where -> São equivalentes aos comandos do SQL
+        user = session.scalar(select(User).where(User.username == 'Gustavo'))
 
-    assert user.username == 'Gustavo'
+        assert asdict(user) == {
+            'id': 1,
+            'username': 'Gustavo',
+            'email': 'gustavo@email.com',
+            'password': 'secret',
+            'created_at': time,
+        }
